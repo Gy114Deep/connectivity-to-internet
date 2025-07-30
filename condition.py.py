3397202c471a4f245cbdb5d220c1419e4c80f6b4
Gyandeep D. <222758102+Gy114Deep@users.noreply.github.com>
2025-07-30 12:44:53 +0530

    Update condition.py.py

--- condition.py.py
+++ condition.py.py
@@ -10,14 +10,3 @@ print(status)
 import datetime
 dt=datetime.datetime.now()
 print(dt.strftime("%I:%M"))
-def mathematics():
-    a=int(input(any))
-    b=int(input(any))
-    c=input("symbol")
-    if c=="/": print(a/b)
-    elif c=="x": print(f"{a}x{b}={a*b}")
-    elif c=="+": print(a+b)
-    elif c=="-": print(a-b)
-    else: 
-        print("define what to do")
-        pass
